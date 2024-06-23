f06c621bffea32927c82eb48330045d2f3de0cc2
MaksymalnyMaks <maksymilian.witczak01@gmail.com>
2024-06-24 00:41:46 +0200

    Updated: This commit includes registration of the order statistics endpoint in run.py.

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from ordermanagment import app, api
 from ordermanagment.resources.order import Order
 from ordermanagment.resources.report import Report
+from ordermanagment.resources.data_analysis import DataAnalysis
 
 
 if __name__ == '__main__':
     api.add_resource(Order, "/backend/<int:order_id>")
     api.add_resource(Report, "/generate_report")
+    api.add_resource(DataAnalysis, '/order_statistics')
     app.run(debug=True)
